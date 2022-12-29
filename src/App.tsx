2ee44b8f1f01dc574e3dd3a8e9d7d873454980ed
Leonardo Massaroli <leoflood98@gmail.com>
2022-12-29 22:43:05 +0000

    Created by component added to the menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./App.css";
+import CreatedBy from "./components/created-by";
 
 // Views
 import GameView from "./views/game";
 import HomeView from "./views/home";
 
 function App() {
-
   return (
     <div className="App">
       <h1>Scissors War</h1>
@@ -19,7 +19,7 @@ function App() {
         </Routes>
       </Router>
 
-      <div className="Game" />
+      <CreatedBy />
     </div>
   );
 }
